a519b63f0dff111b6d74710d719d82e1305e1043
danderson11235 <danderson11235@gmail.com>
2020-11-28 20:38:55 -0500

    Initial commit

---
+++ AdventureLog.cpp
@@ -0,0 +1,12 @@
+﻿// AdventureLog.cpp : Defines the entry point for the application.
+//
+
+#include "AdventureLog.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
