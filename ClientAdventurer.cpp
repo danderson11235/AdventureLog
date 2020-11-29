187a05cb99a91fa25d14daf1e2926e25b82a5e9c
danderson11235 <danderson11235@gmail.com>
2020-11-29 11:16:19 -0500

    Build

---
+++ ClientAdventurer.cpp
@@ -0,0 +1,10 @@
+#include "ClientAdventurer.h"
+#include <WS2tcpip.h>
+
+ClientAdventurer::ClientAdventurer()
+{
+}
+
+ClientAdventurer::~ClientAdventurer()
+{
+}
---
+++ ClientAdventurer.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+class ClientAdventurer
+{
+public:
+	ClientAdventurer();
+	~ClientAdventurer();
+
+private:
+
+};
